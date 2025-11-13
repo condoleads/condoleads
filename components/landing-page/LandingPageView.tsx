11add8706d464cf7833f0b741203e8de0abab288
condoleads <condoleads@gmail.com>
2025-11-13 11:08:02 -0500

    checkpoint: before fixing landing page 404 issue

---
+++ components/landing-page/LandingPageView.tsx
@@ -0,0 +1,27 @@
+import LandingHeader from '@/components/landing/LandingHeader'
+import HeroSection from '@/components/landing/HeroSection'
+import EstimatorDemo from '@/components/landing/EstimatorDemo'
+import PipelineFlow from '@/components/landing/PipelineFlow'
+import BeforeAfter from '@/components/landing/BeforeAfter'
+import PreviewGenerator from '@/components/landing/PreviewGenerator'
+import FeatureCards from '@/components/landing/FeatureCards'
+import DemoEmbed from '@/components/landing/DemoEmbed'
+import CommunityApplication from '@/components/landing/CommunityApplication'
+
+export function LandingPageView() {
+  return (
+    <>
+      <LandingHeader />
+      <main className="pt-16">
+        <HeroSection />
+        <EstimatorDemo />
+        <PipelineFlow />
+        <BeforeAfter />
+        <PreviewGenerator />
+        <FeatureCards />
+        <DemoEmbed />
+        <CommunityApplication />
+      </main>
+    </>
+  )
+}
