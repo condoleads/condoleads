271027cafa447e89d919f0716fe23060f121bcb6
condoleads <condoleads@gmail.com>
2025-12-30 13:51:16 -0500

    fix: add id field to media selection in DevelopmentPage

--- app/[slug]/DevelopmentPage.tsx
+++ app/[slug]/DevelopmentPage.tsx
@@ -105,7 +105,7 @@ export default async function DevelopmentPage({ params, development }: Developme
 
   const { data: allListings } = await serverSupabase
     .from('mls_listings')
-    .select('id, building_id, listing_id, listing_key, standard_status, transaction_type, list_price, close_price, unit_number, unparsed_address, bedrooms_total, bathrooms_total_integer, property_type, living_area_range, square_foot_source, parking_total, locker, association_fee, tax_annual_amount, days_on_market, listing_contract_date, media (media_url, variant_type, order_number, preferred_photo_yn)')
+    .select('id, building_id, listing_id, listing_key, standard_status, transaction_type, list_price, close_price, unit_number, unparsed_address, bedrooms_total, bathrooms_total_integer, property_type, living_area_range, square_foot_source, parking_total, locker, association_fee, tax_annual_amount, days_on_market, listing_contract_date, media (id, media_url, variant_type, order_number, preferred_photo_yn)')
     .in('building_id', buildingIds)
     .order('list_price', { ascending: false })
 
